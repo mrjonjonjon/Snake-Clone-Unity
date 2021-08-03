837ef091e42899c7be7417f8207da20a077927d7
mrjonjonjon <86944352+mrjonjonjon@users.noreply.github.com>
2021-08-03 13:49:03 -0700

    Initial commit

---
+++ Assets/gamemanager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class gamemanager : MonoBehaviour
+{
+    public GameObject clone;
+    public GameObject food;
+    public float increment=0.1f;
+public static gamemanager sharedinstance=null;
+public Text scoretext;
+float score=0f;
+    void Awake(){
+
+          if(sharedinstance!=null && sharedinstance!=this){
+            Destroy(gameObject);
+        }else{
+
+        sharedinstance=this;
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+public void spawnFood(){
+ GameObject newfood=Instantiate(food);
+ newfood.transform.position=new Vector3(Random.Range(-8f,8f),Random.Range(-5f,5f));
+
+}
+    // Update is called once per frame
+    void Update()
+    {increaseScore(increment);
+       scoretext.text="SCORE:"+score.ToString();
+        //food.transform.position=
+    }
+
+    void increaseScore(float increment){
+        score+=increment;
+    }
+
+    public void Clone(){
+        Instantiate(clone);
+    }
+}
---
+++ Assets/playercontrol.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrol : MonoBehaviour
+{
+      
+      static Vector3 direction=new Vector3(1,0,0);
+      public int snakelength=3;
+      public float stepsize=2f;
+      public float tickspeed=0.2f;
+      public float boostspeed=0.05f;
+      //public static playercontrol sharedinstance=null;
+
+     
+    public List<Transform> bodypool;
+    public Transform snakesegment;
+
+    
+    void Awake()
+    {
+
+        
+
+         if (true)
+             {
+                bodypool = new List<Transform>();
+             }
+                for(int i=0;i<snakelength;i++){
+                    bodypool.Add(Instantiate(snakesegment));
+                }
+            
+
+             
+
+    }
+
+    void Start()
+    {  
+        
+        StartCoroutine(moveSnake());
+       
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D col){
+             
+if(col.gameObject.CompareTag("food")){
+    
+    Grow();
+    Destroy(col.gameObject);
+    gamemanager.sharedinstance.spawnFood();
+    gamemanager.sharedinstance.Clone();
+}
+
+
+    }
+
+void OnCollisionEnter2D(Collision2D col){
+
+
+
+
+}
+
+
+    void Update(){
+        if(!gameObject.CompareTag("clone")){
+
+if(Input.GetKey(KeyCode.LeftShift)){
+    tickspeed=boostspeed;
+    //gamemanager.sharedinstance.increment=0.2f;
+}else{
+    tickspeed=0.2f; 
+  // gamemanager.sharedinstance.increment=0.1f;
+}
+    if (Input.GetKeyDown(KeyCode.LeftArrow))
+    {
+       
+        direction=new Vector3(-1,0,0);
+       
+    }else if(Input.GetKeyDown(KeyCode.RightArrow)){
+
+        direction=new Vector3(1,0,0);
+
+
+    }else if(Input.GetKeyDown(KeyCode.UpArrow)){
+        direction=new Vector3(0,1,0);
+
+
+    }else if(Input.GetKeyDown(KeyCode.DownArrow)){
+
+        direction=new Vector3(0,-1,0);
+
+    }
+
+        }
+
+
+    }
+
+IEnumerator moveSnake(){
+        while(true){
+
+          if(bodypool.Count!=0){
+ for(int i=0;i<bodypool.Count-1;i++){
+                bodypool[i].position=bodypool[i+1].position;
+            }
+            bodypool[bodypool.Count-1].position=transform.position;
+          }
+           
+
+            transform.position+=direction*stepsize;
+           // Grow();
+            yield return new WaitForSeconds(tickspeed);
+        }
+
+}
+   
+
+   void Grow(){
+       Transform newseg=Instantiate(snakesegment);
+       newseg.transform.position=bodypool[0].position;
+       bodypool.Insert(0,newseg);
+
+   }
+}
+
